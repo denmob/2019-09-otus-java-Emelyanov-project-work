8afb4c78b328606aff4058e53ff7cb9a25a562db
Denis <mwarez@yahoo.com>
2020-02-23 15:04:25 +0200

    add test for serialize mt

---
+++ sharedLibrary/src/test/java/ru/otus/pw/library/misc/SerializeMessageTransportTest.java
@@ -0,0 +1,32 @@
+package ru.otus.pw.library.misc;
+
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.pw.library.mesages.CommandType;
+import ru.otus.pw.library.mesages.MessageTransport;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class SerializeMessageTransportTest {
+
+    private static Logger logger = LoggerFactory.getLogger(SerializeMessageTransportTest.class);
+
+    @Test
+    void serializeObject() {
+        MessageTransport messageTransport = new MessageTransport("from","to", CommandType.CREATE_PRIVATE_KEY,null);
+        byte[] serializeMT = SerializeMessageTransport.serializeObject(messageTransport);
+        assertNotNull(serializeMT);
+        logger.info(new String(serializeMT));
+    }
+
+    @Test
+    void deserializeBytes() {
+        MessageTransport messageTransport = new MessageTransport("from","to", CommandType.CREATE_PRIVATE_KEY,null);
+        byte[] serializeMT = SerializeMessageTransport.serializeObject(messageTransport);
+        MessageTransport messageTransportNew = SerializeMessageTransport.deserializeBytes(serializeMT);
+        assertEquals(messageTransport.toString(),messageTransportNew.toString());
+    }
+}
--- webOTPService/src/test/java/ru/otus/pw02/mq/MqHandlerImplTest.java
+++ webOTPService/src/test/java/ru/otus/pw02/mq/MqHandlerImplTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.otus.pw.library.mq.MqHandler;
+import ru.otus.pw.library.mq.MqHandlerImpl;
 
 import java.util.stream.IntStream;
 
